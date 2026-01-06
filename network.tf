b9bdfbe25f20b2bfbc37695b6060fc2840b91434
Evan Pincham <TheVirtualMachinist@protonmail.com>
2026-01-05 19:06:38 -0600

    cloud nat

--- network.tf
+++ network.tf
@@ -28,6 +28,8 @@ resource "google_compute_firewall" "ssh_ingress" {
     ports    = ["22"]
   }
 
+  
+
   source_ranges = [var.bastion_allowed_cidr]
   target_tags   = ["bastion"]
 }
@@ -46,3 +48,30 @@ resource "google_compute_firewall" "ssh_internal" {
   source_ranges = ["10.10.0.0/16"]
   target_tags   = ["app"]
 }
+
+
+# Cloud Router (required for Cloud NAT)
+resource "google_compute_router" "router" {
+  name    = "lab-router"
+  region  = var.region
+  network = google_compute_network.vpc.id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+# Cloud NAT (allows private VMs to access internet)
+resource "google_compute_router_nat" "nat" {
+  name                               = "lab-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
